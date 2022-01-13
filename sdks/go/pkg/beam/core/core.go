c9b0f02d5fae288e7cc80ef6dab54dd29d30a8f4
Danny McCormick <damccorm@github.com>
2022-01-13 13:27:57 -0500

    Update core.go

--- sdks/go/pkg/beam/core/core.go
+++ sdks/go/pkg/beam/core/core.go
@@ -21,6 +21,7 @@
 //
 // Files in this package may be generated or updated by release scripts, allowing
 // for accurate version information to be included.
+// TEST - Don't merge this change
 package core
 
 const (
